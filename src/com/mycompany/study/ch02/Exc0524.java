9603906f0f2829c32f0cabed6c6ce58713125998
excitingmvr <excitingmvr@gmail.com>
2023-05-25 16:52:04 +0900

    f.ch02.first

---
+++ src/com/mycompany/study/ch02/Exc0524.java
@@ -0,0 +1,11 @@
+package com.mycompany.study.ch02;
+
+public class Exc0524 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		
+	}
+
+}
---
+++ src/com/mycompany/study/ch02/HelloWorld.java
@@ -0,0 +1,32 @@
+package com.mycompany.study.ch02;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HelloWorld");
+		
+		// 변수 선언 : 데이터타입 변수명; 
+		// 변수 초기화: 데이터타입 변수명 = 리터럴;
+		
+		boolean a = true;
+		boolean b = false;
+		
+		int c = 1;
+		int d;
+		d = 1;
+		System.out.print(b);
+		int e,f,g;
+		e = 10;
+		System.out.println("e: " + e);
+		
+		
+		
+		
+//		System.out.println("c: "+ c);
+		
+		 
+	}
+
+}
