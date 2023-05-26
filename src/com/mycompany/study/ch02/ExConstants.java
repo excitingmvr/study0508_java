5b575440a57cdd649e842e18cf52156b77d3edbc
excitingmvr <excitingmvr@gmail.com>
2023-05-26 10:51:01 +0900

    constants

---
+++ src/com/mycompany/study/ch02/ExConstants.java
@@ -0,0 +1,18 @@
+package com.mycompany.study.ch02;
+
+public class ExConstants {
+
+	public static final String MY_NAME = "Tony";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		public static final String MY_NAME = "Tony";
+		
+		System.out.println("MY_NAME: " + MY_NAME);
+		
+//		MY_NAME = "Andrew";
+		
+	}
+
+}
