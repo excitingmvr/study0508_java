0c18b1afaa66aacde4b7edd21a657c9e2b754f41
excitingmvr <excitingmvr@gmail.com>
2023-06-14 17:28:15 +0900

    f.

---
+++ src/com/mycompany/study/ch08/first/SettopBox.java
@@ -0,0 +1,22 @@
+package com.mycompany.study.ch08.first;
+
+public class SettopBox implements RemoteControl {
+
+	@Override
+	public void turnOn() {
+		System.out.println("SettopBox를 켭니다.");
+	}
+
+	@Override
+	public void turnOff() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setVolume(int volume) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/mycompany/study/ch08/second/InterfaceB.java
@@ -0,0 +1,6 @@
+package com.mycompany.study.ch08.second;
+
+public interface InterfaceB {
+
+	void methodB();
+}
